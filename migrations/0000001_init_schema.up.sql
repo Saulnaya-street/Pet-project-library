7f7f7430d22fe9b08bf8bc5c00bd1306ae919a74
Shelby <119420587+Saulnaya-street@users.noreply.github.com>
2025-03-19 00:05:28 +0700

    fixed the error3

--- migrations/0000001_init_schema.up.sql
+++ migrations/0000001_init_schema.up.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users (
-                        id UUID PRIMARY KEY,
-                        username VARCHAR(255) NOT NULL UNIQUE,
-                        email VARCHAR(255) NOT NULL UNIQUE,
-                        password_hash VARCHAR(255) NOT NULL,
-                        is_admin BOOLEAN NOT NULL DEFAULT FALSE
+                       id UUID PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL UNIQUE,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE books (
-                      id UUID PRIMARY KEY,
-                      genre VARCHAR(255) NOT NULL,
-                      name VARCHAR(255) NOT NULL,
-                      author VARCHAR(255) NOT NULL,
-                      year INTEGER NOT NULL CHECK (year >= 0)
+                       id SERIAL PRIMARY KEY,
+                       genre VARCHAR(255) NOT NULL,
+                       name VARCHAR(255) NOT NULL,
+                       author VARCHAR(255) NOT NULL,
+                       year INTEGER NOT NULL CHECK (year >= 0)
     );
 
 CREATE TABLE user_book (
-                           user_id UUID REFERENCES "user"(id),
-                           book_id UUID REFERENCES book(id),
+                           user_id UUID REFERENCES users(id),
+                           book_id UUID REFERENCES books(id),
                            PRIMARY KEY (user_id, book_id)
-);
+    );
 
 CREATE INDEX idx_book_author ON books(author);
 CREATE INDEX idx_book_year ON books(year);
