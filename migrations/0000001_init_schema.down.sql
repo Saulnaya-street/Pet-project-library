e7797df979d3d044c5cf3bc7c3bded7759a5ef70
Shelby <119420587+Saulnaya-street@users.noreply.github.com>
2025-03-19 02:22:39 +0700

    fixed the error6

--- migrations/0000001_init_schema.down.sql
+++ migrations/0000001_init_schema.down.sql
@@ -1,8 +1,6 @@
 DROP INDEX IF EXISTS idx_book_author;
 DROP INDEX IF EXISTS idx_book_year;
 DROP INDEX IF EXISTS idx_book_genre;
-DROP INDEX IF EXISTS idx_user_book_user;
-DROP INDEX IF EXISTS idx_user_book_book;
 DROP TABLE IF EXISTS user_book;
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
